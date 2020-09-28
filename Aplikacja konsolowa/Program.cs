8223fb1277f1413f3837b0498699f0f78f37ec29
Scooon <scooon@ymail.com>
2020-09-28 19:25:56 +0200

    Zajęcia 2

--- Aplikacja konsolowa/Program.cs
+++ Aplikacja konsolowa/Program.cs
@@ -10,20 +10,33 @@ namespace Aplikacja_konsolowa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj liczbę");
-            int numer = Int32.Parse(Console.ReadLine());
-
-
-            if (numer <= 50)
+            try
             {
-                Console.WriteLine("Tak! Liczba jest mniejsza lub równa 50");
+                Console.WriteLine("Podaj liczbę");
+                int numer = Int32.Parse(Console.ReadLine());
+
+
+                if (numer <= 50)
+                {
+                    Console.WriteLine("Tak! Liczba jest mniejsza lub równa 50");
+                }
+                else
+                {
+                    Console.WriteLine("Nie! Liczba jest większa od 50");
+                }
+
+                Console.ReadLine();
             }
-            else
+            catch
             {
-                Console.WriteLine("Nie! Liczba jest większa od 50");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Wpisana wartość musi być cyfrą!");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Main(args);
             }
 
-            Console.ReadLine();
             Main(args);
         }
     }
---
+++ Szyfrator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szyfrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst");
+            string tekst = Console.ReadLine();
+
+            tekst = tekst.Replace("a", "@");
+
+            Console.WriteLine(tekst);
+
+            Console.ReadKey();
+        }
+    }
+}
