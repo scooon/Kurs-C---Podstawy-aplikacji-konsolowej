f58656c9ad9489aba2bdd9fa0de9bb075303717d
Scooon <scooon@ymail.com>
2020-09-28 18:26:31 +0200

    Initial Commit

---
+++ Aplikacja konsolowa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_konsolowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            int numer = Int32.Parse(Console.ReadLine());
+
+
+            if (numer <= 50)
+            {
+                Console.WriteLine("Tak! Liczba jest mniejsza lub równa 50");
+            }
+            else
+            {
+                Console.WriteLine("Nie! Liczba jest większa od 50");
+            }
+
+            Console.ReadLine();
+            Main(args);
+        }
+    }
+}
