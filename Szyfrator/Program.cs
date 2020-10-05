802273a097224285338bad9cd2eba7a8ecc68b2c
Scooon <scooon@ymail.com>
2020-10-05 19:47:06 +0200

    Zajecia 3

--- Szyfrator/Program.cs
+++ Szyfrator/Program.cs
@@ -8,16 +8,36 @@ namespace Szyfrator
 {
     class Program
     {
+        public static char[,] tablica = new char[,] 
+        {
+        {'a', '@' },
+        {'s', '$' },
+        {'i', '!' },
+        {'e', '3' }
+        
+        };
         static void Main(string[] args)
         {
+            
+            
+            
             Console.WriteLine("Podaj tekst");
             string tekst = Console.ReadLine();
 
-            tekst = tekst.Replace("a", "@");
+
+            for (int i = 0; i < (tablica.Length / 2); i++)
+            {
+                Console.Write("Zamieniam " + tablica[i, 0]);
+                Console.WriteLine(" na " + tablica[i, 1]);
+                tekst = tekst.Replace(tablica[i, 0], tablica[i, 1]);
+
+            }
+
+            
 
             Console.WriteLine(tekst);
 
-            Console.ReadKey();
+            Main(args);
         }
     }
 }
