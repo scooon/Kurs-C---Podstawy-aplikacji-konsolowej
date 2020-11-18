5077ca75983b6ee5e6a5e0ed23d47bb597c029d2
Scooon <scooon@ymail.com>
2020-11-18 18:32:07 +0100

    18.11.2020 18:31

---
+++ C# grupa sroda/Szyfrator/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szyfrator
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hej!", "Powitanie!", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ C# grupa sroda/Szyfrator/PasswordBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szyfrator
+{
+    public partial class PasswordBox : Form
+    {
+        public PasswordBox()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (passwordTextBox.Text == "test")
+            {
+                Console.WriteLine("Zalogowano!");
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Błędne hasło!", "Wpisane hasło jest nieprawidłowe!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if(result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void PasswordBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
