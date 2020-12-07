b57c2ce2d0aa0180d97c95a917202c836f070f79
Scooon <scooon@ymail.com>
2020-12-07 19:20:09 +0100

    7.12.2020 19:20

--- SzyfratorUI/Form1.cs
+++ SzyfratorUI/Form1.cs
@@ -16,6 +16,10 @@ namespace SzyfratorUI
 {
     public partial class Szyfrator : Form
     {
+        byte[] key = new byte[] { 18, 1, 62, 248, 116, 173, 190, 231, 129, 217, 29, 198, 214, 198, 110, 37, 137, 10, 7, 10, 224, 107, 20, 147, 20, 17, 254, 231, 155, 38, 111, 6 };
+        byte[] iv = new byte[] { 251, 206, 128, 185, 18, 70, 175, 117, 81, 51, 116, 212, 182, 23, 144, 197, 95, 225, 96, 78, 148, 122, 191, 90, 95, 232, 52, 104, 203, 11, 146, 72 };
+        string fileContent = string.Empty;
+        string filePath = string.Empty;
 
         public Szyfrator()
         {
@@ -29,24 +33,13 @@ namespace SzyfratorUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            byte[] key = Bytes.GenerateKey();
-            byte[] iv = Bytes.GenerateIV();
-            //byte[] key= new byte[] { 18, 1, 62, 248, 116, 173, 190, 231, 129, 217, 29, 198, 214, 198, 110, 37, 137, 10, 7, 10, 224, 107, 20, 147, 20, 17, 254, 231, 155, 38, 111, 6 };
-            //byte[] iv = new byte[] { 251, 206, 128, 185, 18, 70, 175, 117, 81, 51, 116, 212, 182, 23, 144, 197, 95, 225, 96, 78, 148, 122, 191, 90, 95, 232, 52, 104, 203, 11, 146, 72 };
+            //byte[] key = Bytes.GenerateKey();
+            //byte[] iv = Bytes.GenerateIV();
 
-            for (int i = 0; i < 32; i++)
-            {
-                Console.Write(iv[i] + ",");
-            }
-            Console.WriteLine("}");
 
-            string encrypted = Strings.Encrypt("Scooon", key, iv);
-            string decrypted = Strings.Decrypt("xxWq9yQeCJYgGXY+2zB0QfJ0LyrOoAz6ho0YV6VGFmQ=", key, iv);
-            Console.WriteLine(encrypted);
-            Console.WriteLine(decrypted);
 
-            /*string fileContent = string.Empty;
-            string filePath = string.Empty;
+
+            
 
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
@@ -59,21 +52,22 @@ namespace SzyfratorUI
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
 
+                    //Get the path of specified file
                     filePath = openFileDialog.FileName;
 
-
+                    //Read the contents of the file into a stream
                     var fileStream = openFileDialog.OpenFile();
 
-                    using (MemoryStream ms = new MemoryStream())
+                    using (StreamReader reader = new StreamReader(fileStream))
                     {
-                        fileStream.CopyTo(ms);
-                        deszyfruj(ms.ToArray());
+                        fileContent = reader.ReadToEnd();
                     }
+
+                    Content.Text = Strings.Decrypt(fileContent, key, iv);
                 }
             }
-            */
-            //MessageBox.Show(fileContent, "Zawartość pliku: " + filePath, MessageBoxButtons.OK);
-
+            
+            
 
         }
 
@@ -90,9 +84,9 @@ namespace SzyfratorUI
         private void button2_Click(object sender, EventArgs e)
         {
 
-   
+            
 
-            /*
+            
             Stream myStream;
 
             
@@ -104,6 +98,8 @@ namespace SzyfratorUI
             saveFileDialog1.Title = "Szyfrator - Zapisz plik";
             saveFileDialog1.RestoreDirectory = true;
 
+            byte[] buffer = Encoding.UTF8.GetBytes(Strings.Encrypt(Content.Text, key, iv));
+
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 if ((myStream = saveFileDialog1.OpenFile()) != null)
@@ -111,7 +107,7 @@ namespace SzyfratorUI
                     myStream.Write(buffer, 0, buffer.Length);
                     myStream.Close();
                 }
-            } */
+            } 
         }
 
         
